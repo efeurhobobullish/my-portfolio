65d3d809e44864543b7b40ef7d1664a0516f8d46
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-16 11:47:26 +0100

    Update ProjectsCard.jsx

--- src/Components/Projects/ProjectsCard.jsx
+++ src/Components/Projects/ProjectsCard.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import styles from "../Styles/projects.module.css";
 import { motion } from "framer-motion";
+
 const ProjectsCard = ({
   image,
   title,
@@ -10,28 +11,45 @@ const ProjectsCard = ({
   github,
 }) => {
   return (
-    <motion.li className={styles.item}>
+    <motion.li
+      className={styles.item}
+      whileHover={{ y: -6, scale: 1.02 }}
+      transition={{ duration: 0.2 }}
+    >
       <img src={image} alt={title} />
+
       <div className={styles.body}>
         <h3>{title}</h3>
         <p>{description}</p>
+
         <ul>
           {stack.map((tech, index) => (
             <li key={index}>#{tech}</li>
           ))}
         </ul>
       </div>
+
       <div className={styles.btns}>
         {live_link && (
-            <a href={live_link} className={styles.btn_1} target="_blank">
-              Live demo &nbsp;{" "}
-              <i className="fa-solid fa-arrow-up-right-from-square"></i>
-            </a>
+          <a
+            href={live_link}
+            className={styles.btn_1}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Live Demo <i className="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
         )}
+
         {github && (
-            <a href={github} className={styles.btn_2} target="_blank">
-              GitHub &nbsp; <i className="fa-brands fa-github-alt"></i>
-            </a>
+          <a
+            href={github}
+            className={styles.btn_2}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub <i className="fa-brands fa-github"></i>
+          </a>
         )}
       </div>
     </motion.li>
